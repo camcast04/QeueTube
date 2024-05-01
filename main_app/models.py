1545f6c09ffe63e9cee83bba451660a806069fb6
camcast04 <cam.cast04@gmail.com>
2024-05-01 06:07:02 -0400

    setting up video and playlist models

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Playlist(models.Model):
+    title = models.CharField(max_length=255)
+    
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    youtube_id = models.CharField(max_length=255)
+    # one playlist can hold many videos 
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
