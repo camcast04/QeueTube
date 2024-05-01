05e2468ae715bdf6e2d1564b078baeedfb035df3
camcast04 <cam.cast04@gmail.com>
2024-05-01 01:55:00 -0400

    switching to bootstrap

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views # . means from current file (main_app)
 
 
+
+
 urlpatterns = [
   path('', views.home, name='home'), #making home controller -> now need to create the homes view in views.py
 ]
